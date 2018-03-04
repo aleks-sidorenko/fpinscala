19aac8eb13d4b800f26a2090b8015f0831ecc6c1
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2018-03-04 23:06:16 +0200

    [datastructures] foldLeft, sum, product

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -8,16 +8,11 @@ which may be `Nil` or another `Cons`.
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List { // `List` companion object. Contains functions for creating and working with lists.
-  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
-    case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
-  }
+  def sum(ints: List[Int]): Int =
+    foldLeft(ints, 0)(_ + _)
 
-  def product(ds: List[Double]): Double = ds match {
-    case Nil => 1.0
-    case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
-  }
+  def product(ds: List[Double]): Double =
+    foldLeft(ds, 1.0)(_ * _)
 
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
@@ -82,7 +77,12 @@ object List { // `List` companion object. Contains functions for creating and wo
   def length[A](l: List[A]): Int = 
     foldRight(l, 0)((_, acc) => acc + 1)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = 
+    l match {
+      case Cons(h, t) => foldLeft(t, f(z, h))(f)
+      case Nil => z
+    }
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 }
