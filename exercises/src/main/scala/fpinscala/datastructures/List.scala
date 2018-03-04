4e14c5c1909db10a1898e47d633979f04ac0079b
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2018-03-04 22:34:01 +0200

    [datastructures] init

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -71,7 +71,13 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Nil => Nil
     }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = 
+    l match {
+      case Cons(_, Nil) => Nil
+      case Cons(x, xs) => Cons(x, init(xs))
+      case Nil => Nil
+      
+    }
 
   def length[A](l: List[A]): Int = ???
 
