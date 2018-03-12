315236ca1051e009eb5f6744eddbf3a2fdf88204
Aleksandr Sidorenko <aleks.sidorenko@gmail.com>
2018-03-12 13:02:16 +0200

    [datastructures] depth

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -4,26 +4,35 @@ sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
-
 object Tree {
-
 
   def size[A](tree: Tree[A]): Int = {
     tree match {
-        case null => 0
-        case Leaf(_) => 1
-        case Branch(l, r) => 1 + size(l) + size(r)
-      }
+      case null         => 0
+      case Leaf(_)      => 1
+      case Branch(l, r) => 1 + size(l) + size(r)
+    }
   }
 
   def maximum(tree: Tree[Int]): Int = {
-    def go(tree: Tree[Int], max: Int): Int = 
+    def go(tree: Tree[Int], max: Int): Int =
       tree match {
-          case null => max
-          case Leaf(v: Int) => math.max(max, v)
-          case Branch(l, r) => math.max(go(l, max), go(r, max))
-        }
-    
+        case null         => max
+        case Leaf(v: Int) => math.max(max, v)
+        case Branch(l, r) => math.max(go(l, max), go(r, max))
+      }
+
+    go(tree, 0)
+  }
+
+  def depth(tree: Tree[Int]): Int = {
+    def go(tree: Tree[Int], acc: Int): Int =
+      tree match {
+        case null         => acc
+        case Leaf(_)      => acc + 1
+        case Branch(l, r) => math.min(go(l, acc + 1), go(r, acc + 1))
+      }
+
     go(tree, 0)
   }
 
