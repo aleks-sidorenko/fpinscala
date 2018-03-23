712695cec9f987d60c05866c0751d74741aa2116
Aleksandr Sidorenko <aleks.sidorenko@gmail.com>
2018-03-23 12:09:03 +0200

    [errorhandling] Either -> map2

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -22,7 +22,12 @@ sealed trait Either[+E, +A] {
     case _       => this
   }
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def map2[EE >: E, B, C](b: Either[EE, B])(
+      f: (A, B) => C): Either[EE, C] = 
+      for {
+        av <- this
+        bv <- b
+      } yield f(av, bv)
 }
 case class Left[+E](get: E) extends Either[E, Nothing]
 case class Right[+A](get: A) extends Either[Nothing, A]
