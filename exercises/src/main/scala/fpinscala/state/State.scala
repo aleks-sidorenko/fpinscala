4d755a0c53fe8fa584d57795b8bcad033f245b55
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2018-08-19 23:18:15 +0300

    [state] RNG -> map, map2 via flatMap

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -25,10 +25,7 @@ object RNG {
     rng => (a, rng)
 
   def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
-    rng => {
-      val (a, rng2) = s(rng)
-      (f(a), rng2)
-    }
+    flatMap(s) { a => unit(f(a)) }
 
   def positiveInt(rng: RNG): (Int, RNG) = { 
     val (a, rng2) = rng.nextInt
@@ -71,12 +68,7 @@ object RNG {
     
 
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = 
-    rng => {
-      val (a, rng2) = ra(rng)
-      val (b, rng3) = rb(rng2)
-      f(a, b) -> rng3
-    }
-
+    flatMap(ra) { a => flatMap(rb) { b => unit(f(a, b)) } }
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = 
     fs.foldRight(unit[List[A]](List.empty[A])) { (f, acc) => map2(f, acc)( _ :: _ ) }
